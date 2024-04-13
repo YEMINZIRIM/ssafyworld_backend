66d4da0e989d9cd25c9e434e22ee8f72cc9b8318
ma-an-jong <alswhd1113@gmail.com>
2024-04-13 20:18:55 +0900

    🌠project init

---
+++ src/main/java/com/yeminjilim/ssafyworld/SsafyworldApplication.java
@@ -0,0 +1,13 @@
+package com.yeminjilim.ssafyworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsafyworldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsafyworldApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yeminjilim/ssafyworld/SsafyworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeminjilim.ssafyworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsafyworldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
