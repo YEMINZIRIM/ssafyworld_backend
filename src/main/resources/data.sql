4fa59e8c26e44c19a68b659a001bad62d44f36bd
ma-an-jong <alswhd1113@gmail.com>
2024-04-13 20:55:08 +0900

    chore : data.sql에 init data를 추가
    각자 개발 환경에서 필요한 data를 추가하기 위한 sql을 작성하는 파일 추가
    
    Resolves:
    Related to:

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO group_info(id, ordinal,region,ban) values(1,11,'구미',4);
+
+INSERT INTO member(id,sub,provider,groupInfoId,name,serialNumber) values(10001, '102828872935030969646' , 'google', 1, '김민종','KIMMINJONG');
+INSERT INTO member(id,sub,provider,groupInfoId,name,serialNumber) values(10002, '113049760373373067515' , 'google', 1, '이지언','LEEJIEON');
+INSERT INTO member(id,sub,provider,groupInfoId,name,serialNumber) values(10003, '113319945388395204387' , 'google', 1, '이예찬','LEEYECHAN');
