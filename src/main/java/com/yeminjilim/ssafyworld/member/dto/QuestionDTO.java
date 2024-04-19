186dc658ec33b09f4e537f45a016d673970f7e8b
ma-an-jong <alswhd1113@gmail.com>
2024-04-19 22:05:46 +0900

    feat: Question 도메인 CRUD 추가
    Question 도메인의 CRUD 로직 추가
    
    Resolves:
    Related to:

---
+++ src/main/java/com/yeminjilim/ssafyworld/member/dto/QuestionDTO.java
@@ -0,0 +1,22 @@
+package com.yeminjilim.ssafyworld.member.dto;
+
+import com.yeminjilim.ssafyworld.member.entity.Question;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Data;
+
+@Builder(access = AccessLevel.PRIVATE)
+@Data
+public class QuestionDTO {
+
+    private Long id;
+    private String question;
+
+    public static QuestionDTO of(Question question) {
+        return builder()
+                .id(question.getId())
+                .question(question.getQuestion())
+                .build();
+    }
+
+}
