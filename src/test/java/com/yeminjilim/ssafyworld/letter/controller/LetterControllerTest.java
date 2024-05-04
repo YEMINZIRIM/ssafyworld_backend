5120f46d28ce3b73cd511230cc980ebc452826ac
Juhyorim <joohr1234@gmail.com>
2024-05-04 18:56:52 +0900

    test: letter 쓰기 길이제한 관련 테스트코드 작성
    letter 쓰기 길이제한 관련 테스트코드 작성
    원하는 길이의 문자열을 생성해주는 테스트 전용 CustomStringUtil 생성
    
    Resolves:
    Related to:

--- src/test/java/com/yeminjilim/ssafyworld/letter/controller/LetterControllerTest.java
+++ src/test/java/com/yeminjilim/ssafyworld/letter/controller/LetterControllerTest.java
@@ -5,6 +5,7 @@ import com.yeminjilim.ssafyworld.letter.dto.LetterDTO.CreateResponse;
 import com.yeminjilim.ssafyworld.letter.dto.LetterDTO.ReceivedLetterResponse;
 import com.yeminjilim.ssafyworld.letter.error.CustomLetterException;
 import com.yeminjilim.ssafyworld.letter.error.LetterErrorCode;
+import com.yeminjilim.ssafyworld.util.CustomStringUtil;
 import java.util.concurrent.atomic.AtomicReference;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Order;
@@ -22,9 +23,13 @@ import reactor.test.StepVerifier;
 @SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
 @AutoConfigureWebTestClient
 public class LetterControllerTest {
-    public static final String TITLE = "title입니다";
-    public static final String CONTENT = "content입니다";
-    public static final long RECEIVER_ID = 2L;
+    public static final int TITLE_MAX_SIZE = 50;
+    public static final int TITLE_GENERAL_SIZE = 15;
+    public static final int CONTENT_MAX_SIZE = 5000;
+    public static final int CONTENT_GENERAL_SIZE = 100;
+    public static String TITLE = "";
+    public static String CONTENT = "";
+    public static long RECEIVER_ID = 2L;
 
     @Autowired
     private WebClient webClient;
@@ -32,6 +37,9 @@ public class LetterControllerTest {
     @Test
     @Order(1)
     public void createLetterTest() {
+        TITLE = CustomStringUtil.makeRandomString(TITLE_GENERAL_SIZE);
+        CONTENT = CustomStringUtil.makeRandomString(CONTENT_GENERAL_SIZE);
+
         Mono<ResponseEntity<CreateResponse>> responseMono = this.webClient
                 .post()
                 .uri("/letter")
@@ -48,6 +56,55 @@ public class LetterControllerTest {
                     Assertions.assertThat(prod.getBody().getToUser()).isEqualTo(RECEIVER_ID);
                 })
                 .verifyComplete();
+    }
+
+    @Test
+    @Order(1)
+    public void createLetter_maxSize() {
+        TITLE = CustomStringUtil.makeRandomString(TITLE_MAX_SIZE);
+        CONTENT = CustomStringUtil.makeRandomString(CONTENT_MAX_SIZE);
+
+        Mono<ResponseEntity<CreateResponse>> responseMono = this.webClient
+                .post()
+                .uri("/letter")
+                .bodyValue(new CreateRequest(RECEIVER_ID, TITLE, CONTENT))
+                .retrieve()
+                .toEntity(CreateResponse.class)
+                .doOnNext(System.out::println);
+
+        StepVerifier.create(responseMono)
+                .assertNext(prod -> {
+                    Assertions.assertThat(prod).isNotNull();
+                    Assertions.assertThat(prod.getBody().getTitle()).isEqualTo(TITLE);
+                    Assertions.assertThat(prod.getBody().getContent()).isEqualTo(CONTENT);
+                    Assertions.assertThat(prod.getBody().getToUser()).isEqualTo(RECEIVER_ID);
+                })
+                .verifyComplete();
+    }
+
+    @Test
+    @Order(1)
+    public void createLetter_overSize() {
+        TITLE = CustomStringUtil.makeRandomString(30);
+        CONTENT = CustomStringUtil.makeRandomString(5001);
+
+        Mono<ResponseEntity<ReceivedLetterResponse>> receivedResponse = this.webClient
+                .post()
+                .uri("/letter")
+                .bodyValue(new CreateRequest(RECEIVER_ID, TITLE, CONTENT))
+                .retrieve()
+                .onStatus(HttpStatus::is4xxClientError, response ->
+                        Mono.error(
+                                new CustomLetterException(
+                                        LetterErrorCode.NOT_FOUND
+                                )
+                        )
+                ).toEntity(ReceivedLetterResponse.class);
+
+        StepVerifier.create(receivedResponse)
+                .expectErrorMatches(response ->
+                        response instanceof CustomLetterException)
+                .verify();
     }
 
     @Test
